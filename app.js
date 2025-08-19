0814304ceb64cf037da7deafda4fdd8b9925b0ac
elij123 <es3313@rit.edu>
2025-08-19 21:41:57 +0530

    Changed to mysql2

--- app.js
+++ app.js
@@ -1,43 +1,21 @@
 import express from "express";
-import mysql from "mysql"
+import mysql from "mysql2/promise"
 
 const app = express()
-
 const port = 3000
 
-const connection = mysql.createConnection({
-    host: "mysql-nulz",
+try {
+    const connection = await mysql.createConnection({
+    host: 'mysql-nulz',
     port: 3306,
-    user: "elijah",
+    user: 'elijah',
     password: process.env.DB_PASSWORD,
-    database: "school"
-})
-
-connection.connect()
-
-const test = (loc1,loc2) => 34
-
-app.get("/listSchools",(req,res) =>{
-    // const latitude = req.body.latitude
-    // const longitude = req.body.longtitude
-    connection.query("SELECT id,latitude,longitude FROM school_list", function (error, results, fields){
-        console.log(results)
-    })
-    // Fetch the list from the MySQL DB and calculate
-    // the distance along with map() and sort it
-    // in ascending order using sort(callback)
-    // const list = ["test"]
-    // res.send(list)
-})
-
-// app.post("/addSchool",(req,res) => {
-//     //Validates values from the request
-
-//     //Adds a row to the relational table 'Schools'
-// })
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
-
-connection.end()
+    database: 'school'
+  });
+  const sql = 'SELECT `id`,`latitude`,`longitude` FROM `school_list`'
+  const [rows, fields] = await connection.query(sql);
+  console.log(rows);
+  console.log(fields);
+} catch (error) {
+    console.log(error);
+}
